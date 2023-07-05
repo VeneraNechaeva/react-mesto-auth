28e60ae8ea3a52857aaee49d9d3e4297e0399c41
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-07-05 09:55:24 +0300

    feat: создать компогент Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import headerLogo from '../images/Vector.svg';
 
 function Register() {
+
+
+    
     return (
-        <div></div>
-        // <header className="header page__margin">
-        //     <img
-        //         className="header__logo"
-        //         src={headerLogo}
-        //         alt="сервис Mesto"
-        //     />
-        // </header>
+        <form className="register__form" name="register" noValidate="">
+            <h2 className="register__title">Регистрация</h2>
+            <div className="register__label">
+                <input
+                    id="email"
+                    className="register__field register__field_text_email"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    minLength={2}
+                    maxLength={40}
+                    required=""
+                />
+                <span className="register__error email-error" />
+            </div>
+            <div className="register__label">
+                <input
+                    id="password"
+                    className="register__field login__field_text_password"
+                    type="password"
+                    name="password"
+                    placeholder="Пароль"
+                    minLength={10}
+                    maxLength={10}
+                    required=""
+                />
+                <span className="register__error password-error" />
+            </div>
+            <button className="register__button" type="submit">
+                Зарегистрироваться
+            </button>
+            <p className="register__text">Уже зарегистрированы? Войти</p>
+        </form>
     )
 }
 
